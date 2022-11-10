9e0737f7836e302605f992a48bbed7a184bf99a6
Tugberk <tugs1995@gmail.com>
2022-11-10 13:24:55 +0300

    refactor and cleanup

---
+++ glov-sdk-react/src/glov-sdk.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { eligibilityCheck, fetchConfig } from "../../glov_core/index"
+import { glovInfoLayerInit } from "../../glov_core/GlovInfoLayer/index";
+  
+const GlovContext = React.createContext({eRule:{}, context:[], robot:{"game_title": "Best Game Ever"}});
+  
+export const Glov = (clientId) => {
+    init(clientId);
+    return GlovContext;
+}
+
+export const GlovProvider = ({children, glov}) => {
+    return (
+        <GlovContext.Provider value={{glov}}>
+            {children}
+        </GlovContext.Provider>
+    )
+}
+
+function robot(id, glovContext) {
+    return glovContext?.robot?.[id] || null;
+  }
+
+export function useRobot(id){
+    console.log("oi!");
+    const { glovContext } = React.useContext(GlovContext);
+    console.log(glovContext);
+  return robot(id, glovContext);
+  }
+
+async function init(clientId) {
+    glovInfoLayerInit()
+    await fetchConfig(clientId);
+    eligibilityCheck();
+}
